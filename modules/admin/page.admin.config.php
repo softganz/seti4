5ed57a91080608a5058ee472bd03e72345275a5b
Little Bear <softganz@users.noreply.github.com>
2022-03-31 22:17:23 +0700

    Convert admin/config to oage widget

--- modules/admin/page.admin.config.php
+++ modules/admin/page.admin.config.php
@@ -1,4 +1,40 @@
 <?php
+/**
+* Module :: Description
+* Created 2022-03-31
+* Modify  2022-03-31
+*
+* @param String $arg1
+* @return Widget
+*
+* @usage module/{id}/method
+*/
+
+class AdminConfig extends Page {
+	var $arg1;
+
+	function __construct($arg1 = NULL) {
+		$this->arg1 = $arg1;
+	}
+
+	function build() {
+		return new Scaffold([
+			'appBar' => new AppBar([
+				'title' => 'Site Configuration',
+				'navigator' => 	R::View('admin.default.nav'),
+			]), // AppBar
+			'body' => new Container([
+				'class' => 'admin-panel',
+				'children' => [
+					R::View('admin.menu.config'),
+				], // children
+			]), // Widget
+		]);
+	}
+}
+?>
+
+<?php
 function admin_config($self) {
 	$self->theme->title='Site Configuration';
 	$ret.='<div class="admin-panel">';
--- modules/admin/view.admin.menu.config.php
+++ modules/admin/view.admin.menu.config.php
@@ -14,30 +14,177 @@
 $debug = true;
 
 function view_admin_menu_config() {
-	$ret = '<header><h3><a href="'.url('admin/config').'">Site Configuration</a></h3></header>';
 
 
-	$ui = new Ui(NULL, 'ui-card');
+	$ui = new Column([
+		'children' => [
+			new ListTile(['title' => 	'Site Configuration']),
+			new Card([
+				'children' => [
+					new ListTile([
+						'crossAxisAlignment' => 'start',
+						'title' => 'Clear user online',
+						'leading' => new Icon('people'),
+						'trailing' => '<a class="btn" href="'.url('admin/config/online').'">Clear user online</a>',
+						'subtitle' => 'Remove all user online item from database.',
+					]),
+				], // children
+			]),
 
-	$ui->add('<a href="'.url('admin/config/online').'">Clear user online</a><p>Remove all user online item from database.</p>');
-	$ui->add('<a href="'.url('admin/config/session/clear').'">Clear empty session</a><p>Remove all empty from database.</p>');
+			new Card([
+				'children' => [
+					new ListTile([
+						'crossAxisAlignment' => 'start',
+						'title' => 'Clear empty session',
+						'leading' => new Icon('groups'),
+						'trailing' => '<a class="btn" href="'.url('admin/config/session/clear').'">Clear empty session</a>',
+						'subtitle' => 'Remove all empty from database.',
+					]),
+				], // children
+			]),
 
-	if (R()->appAgent) {
-		$ui->add('<a class="sg-action" href="'.url('',array('setting:app' => '{}')).'" data-rel="none" data-done="reload"><i class="icon -material">web</i><span>Setting Agent to Desktop Web</span></a><p>Set configuration to be as Web</p>');
-	} else {
-		$ui->add('<a class="sg-action" href="'.url('',array('setting:app' => '{OS:%22Android%22,ver:%220.20.0%22,type:%22App%22,dev:%22Softganz%22}')).'" data-rel="none" data-done="reload"><i class="icon -material">android</i><span>Setting Agent to App</span></a><p>Set configuration to be as App</p>');
-	}
+			new Card([
+				'children' => [
+					new ListTile([
+						'crossAxisAlignment' => 'start',
+						'title' => 'App Agent',
+						'leading' => new Icon(R()->appAgent ? 'android' : 'web'),
+						'trailing' => R()->appAgent ? '<a class="btn" class="sg-action" href="'.url('',array('setting:app' => '{}')).'" data-rel="none" data-done="reload">Mobile App</a>' : '<a class="sg-action btn" href="'.url('',array('setting:app' => '{OS:%22Android%22,ver:%220.20.0%22,type:%22App%22,dev:%22Softganz%22}')).'" data-rel="none" data-done="reload">Web App</a>',
+						'subtitle' => 'Setting Agent'
+					]),
+				], // children
+			]), // Card
 
-	$ui->add('<a href="'.url('admin/config/daykey/clear').'">Clear daykey</a><p>Remove all daykey from database.</p>');
-	$ui->add('<a href="'.url('admin/config/counter').'">Re-build counter</a><p>Re-build counter and write to database.</p>');
-	$ui->add('<a href="'.url('admin/config/dbvar').'">DB variable list</a><p>List of DB config</p>');
-	$ui->add('<a href="'.url('admin/config/phpinfo').'">Server information</a><p>Information for web server and php environment.</p>');
-	$ui->add('<a href="'.url('admin/config/view').'">View configuration</a><p>Display all software configuration.</p>');
-	$ui->add('<a href="'.url('admin/config/cookie').'">View cookies value</a><p>Display all $_COOKIES variable.</p>');
-	$ui->add('<a href="'.url('admin/config/db').'">View db variable</a><p>Display only configuration stroe in table.</p>');
-	$ui->add('<a href="'.url('admin/config/session').'">View session value</a><p>Display all $_SESSION variable.</p>');
+			new Card([
+				'children' => [
+					new ListTile([
+						'title' => 'JS Min',
+						'leading' => new Icon('javascript'),
+						'trailing' => new Row([
+							'children' => [
+								$_SESSION['jsMin'] == 'no' ? '<a class="btn -link" href="'.url('admin/config',['jsMin' => 'clear']).'"><i class="icon -material -gray">toggle_off</i><span>OFF</span></a>' : '<a class="btn -link" href="'.url('admin/config',['jsMin' => 'no']).'"><i class="icon -material -green">toggle_on</i><span>ON</span></a>',
+								// '<a class="btn" href="'.url('admin/config',['jsMin' => 'clear']).'"><i class="icon -material">cancel</i><span>CLEAR</span></a>',
+							]
+						]), // Row
+					]),
+				], // children
+			]),
 
-//setting:app={OS:"Android",ver:"0.20",type:"icar",dev:"Softganz"}
+			new Card([
+				'children' => [
+					new ListTile([
+						'crossAxisAlignment' => 'start',
+						'title' => 'Clear day key',
+						'leading' => new Icon(''),
+						'trailing' => new Row([
+							'child' => '<a class="sg-action btn" href="'.url('admin/config/daykey/clear').'" data-rel="none" data-title="Clear" data-confirm="Clear daykey?">Clear daykey</a>'
+						]),
+						'subtitle' => 'Remove all daykey from database.',
+					]),
+				], // children
+			]), // Card
+
+			new Card([
+				'children' => [
+					new ListTile([
+						'crossAxisAlignment' => 'start',
+						'title' => 'Re-build counter',
+						'leading' => new Icon(''),
+						'trailing' => new Row([
+							'child' => '<a class="btn" href="'.url('admin/config/counter').'">Re-build</a>'
+						]),
+						'subtitle' => 'Re-build counter and write to database.',
+					]),
+				], // children
+			]), // Card
+
+			new Card([
+				'children' => [
+					new ListTile([
+						'crossAxisAlignment' => 'start',
+						'title' => 'DB variable list',
+						'leading' => new Icon(''),
+						'trailing' => new Row([
+							'child' => '<a class="btn" href="'.url('admin/config/dbvar').'">Show</a>'
+						]),
+						'subtitle' => 'List of DB config<',
+					]),
+				], // children
+			]), // Card
+
+			new Card([
+				'children' => [
+					new ListTile([
+						'crossAxisAlignment' => 'start',
+						'title' => 'Server information',
+						'leading' => new Icon(''),
+						'trailing' => new Row([
+							'child' => '<a class="btn" href="'.url('admin/config/phpinfo').'">View</a>'
+						]),
+						'subtitle' => 'Information for web server and php environment.',
+					]),
+				], // children
+			]), // Card
+
+			new Card([
+				'children' => [
+					new ListTile([
+						'crossAxisAlignment' => 'start',
+						'title' => 'View configuration',
+						'leading' => new Icon(''),
+						'trailing' => new Row([
+							'child' => '<a class="btn" href="'.url('admin/config/view').'">View</a>'
+						]),
+						'subtitle' => 'Display all software configuration.',
+					]),
+				], // children
+			]), // Card
+
+			new Card([
+				'children' => [
+					new ListTile([
+						'crossAxisAlignment' => 'start',
+						'title' => 'View cookies value',
+						'leading' => new Icon(''),
+						'trailing' => new Row([
+							'child' => '<a class="btn" href="'.url('admin/config/cookie').'">View</a>'
+						]),
+						'subtitle' => 'Display all $_COOKIES variable.',
+					]),
+				], // children
+			]), // Card
+
+			new Card([
+				'children' => [
+					new ListTile([
+						'crossAxisAlignment' => 'start',
+						'title' => 'View db variable',
+						'leading' => new Icon(''),
+						'trailing' => new Row([
+							'child' => '<a class="btn" href="'.url('admin/config/db').'">View</a>'
+						]),
+						'subtitle' => 'Display only configuration stroe in table.',
+					]),
+				], // children
+			]), // Card
+
+			new Card([
+				'children' => [
+					new ListTile([
+						'crossAxisAlignment' => 'start',
+						'title' => 'View session value',
+						'leading' => new Icon(''),
+						'trailing' => new Row([
+							'child' => '<a class="btn" href="'.url('admin/config/session').'">View</a>'
+						]),
+						'subtitle' => 'Display all $_SESSION variable.',
+					]),
+				], // children
+			]), // Card
+		], // children
+	]);
+
+	//setting:app={OS:"Android",ver:"0.20",type:"icar",dev:"Softganz"}
 	$ret .= $ui->build();
 
 	return $ret;
