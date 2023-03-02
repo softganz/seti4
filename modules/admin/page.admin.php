4eb82125845cf36562f156200cee1d218843a1a4
Little Bear <softganz@users.noreply.github.com>
2023-03-02 15:30:11 +0700

    Add search user form to admin main page

--- modules/admin/page.admin.php
+++ modules/admin/page.admin.php
@@ -27,6 +27,9 @@ class Admin extends Page {
 		return new Scaffold([
 			'appBar' => new AppBar([
 				'title' => 'Web Site Administrator on '.cfg('core.version'),
+				'trailing' => '<form id="search" class="search-box" method="get" action="'.url('admin/user/list').'" name="memberlist" role="search">'
+					. '<input type="hidden" name="sid" id="sid" /><input id="search-box" class="sg-autocomplete" type="text" name="q" size="20" value="'.$_GET['q'].'" data-query="'.url('admin/get/username').'" data-altfld="sid" data-callback="submit" placeholder="Username or Name or Email"><button><i class="icon -material">search</i></button>'
+					. '</form>',
 				'navigator' => 	R::View('admin.default.nav'),
 			]), // AppBar
 			'body' => new Widget([
