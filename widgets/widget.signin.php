c45a0632bf8237ec27ed7fd3996fe7461b64de96
Little Bear <softganz@users.noreply.github.com>
2023-04-20 09:56:31 +0700

    Improve SignInWidget

--- widgets/widget.signin.php
+++ widgets/widget.signin.php
@@ -1,8 +1,8 @@
 <?php
 /**
-* SignIn  :: Description
+* SignIn  :: Sign In Widget
 * Created :: 2022-07-20
-* Modify  :: 2022-10-14
+* Modify  :: 2023-04-11
 * Version :: 2
 *
 * @param Array $args
@@ -11,27 +11,27 @@
 * @usage new SignInWidget([])
 */
 
-$debug = true;
-
 class SignInWidget extends Widget {
+	var $username;
+	var $password;
+	var $time = 10080;
+	var $done;
+	var $showRegisterButton = true;
+	var $showForgotButton = true;
 
 	function __construct($args = []) {
 		parent::__construct($args);
 	}
 
 	function build() {
-		$para = para(func_get_args(), 'class=signform');
-		$para->nocontainer = 'yes';
-
 		return new Form([
-				'action' => SG\getFirst(url(q())),
-				'id' => $para->id ? $para->id : 'signin-'.uniqid(),
-				'class' => $para->class,
-				// $form->config->action=SG\getFirst($_GET['action'],$para->action,url(q()),$_SERVER['HTTP_REFERER']);
-				// $form->config->action=SG\getFirst($_GET['action'],$para->action,_URL,$_SERVER['HTTP_REFERER']);
+				'action' => SG\getFirst($this->returnUrl, url(q())),
+				'id' => $this->id ? $this->id : 'signin-'.uniqid(),
+				'class' => 'widget-signin signform'.($this->class ? ' '.$this->class : ''),
+				'rel' => 'none',
+				'done' => $this->done,
 				'children' => [
 					'username' => [
-						'name' => 'username',
 						'type' => 'text',
 						'id' => 'username-'.uniqid(),
 						'label' => tr('Username').' '.tr('or').' '.tr('e-mail'),
@@ -39,39 +39,42 @@ class SignInWidget extends Widget {
 						'placeholder' => 'Username',
 						'maxlength' => 50,
 						'autocomplete' => 'off',
+						'value' => $this->username,
 						'container' => '{class: "-label-in"}',
 					],
 					'password' => [
-						'name' => 'password',
 						'type' => 'password',
 						'id' => 'password-'.uniqid(),
 						'label' => tr('Password'),
 						'class' => '-password -fill',
 						'placeholder' => 'Password',
 						'maxlength' => 20,
+						'value' => $this->password,
 						'container' => '{class: "-label-in"}',
 					],
-					'cookielength' => [
-						'name' => 'cookielength',
+					'cookielength' => in_array($this->time, [-1, 'forever']) ? [
+						'type' => 'hidden',
+						'value' => '-1',
+					] : [
 						'type' => 'select',
-						'id' => uniqid(),
-						'class' => '-fill',
-						'options' => array(
+						'id' => 'time-'.uniqid(),
+						'class' => '-time -fill',
+						'value' => $this->time,
+						'options' => [
 							'60' => '1 '.tr('Hour'),
 							'1440' => '1 '.tr('Day'),
 							'10080' => '1 '.tr('Week'),
 							'43200' => '1 '.tr('Month'),
 							'-1' => tr('Forever')
-						),
-						'value' => SG\getFirst($para->cookielength, $_POST['cookielength'], 10080)
+						],
 					],
 					'signin' => [
 						'type' => 'button',
 						'name' => 'signin',
 						'value' => '<i class="icon -material">login</i><span>'.tr('Sign in').'</span>',
 					],
-					user_access('register new member') ? '<a class="btn -link" href="'.url('user/register').'"><i class="icon -material">person_add</i><span>'.tr('Create new account').'</span></a>' : NULL,
-					'<a class="btn -link" href="#" onclick="window.location=\''.url('user/password').'\';return false;"><i class="icon -material">restore</i><span>'.tr('Request new password').'?</span></a>',
+					$this->showRegisterButton && user_access('register new member') ? '<a class="btn -link" href="'.url('user/register').'"><i class="icon -material">person_add</i><span>'.tr('Create new account').'</span></a>' : NULL,
+					$this->showForgotButton ? '<a class="btn -link" href="#" onclick="window.location=\''.url('user/password').'\';return false;"><i class="icon -material">restore</i><span>'.tr('Request new password').'?</span></a>' : NULL,
 				], // children
 			]);
 	}
