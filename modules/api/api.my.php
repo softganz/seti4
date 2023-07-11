f1147c872176cbaaa0317ba4abbcf3b06429bcb5
Little Bear <softganz@users.noreply.github.com>
2023-07-11 17:04:27 +0700

    Fixed bug: remove debug

--- modules/api/api.my.php
+++ modules/api/api.my.php
@@ -35,13 +35,13 @@ class MyApi extends PageApi {
 
 		if (!($post->current && $post->password && $post->repassword)) return error(_HTTP_ERROR_NOT_ACCEPTABLE, 'ข้อมูลไม่ครบถ้วน');
 		else if ($post->current === '' ) return error(_HTTP_ERROR_BAD_REQUEST, 'กรุณาระบุรหัสผ่านปัจจุบัน');
-		else if (!($post->current === sg_decrypt($userInfo->password,cfg('encrypt_key')))) return error(_HTTP_ERROR_NOT_ACCEPTABLE, 'รหัสผ่านปัจจุบันไม่ถูกต้อง'.$post->current);
+		else if (!($post->current === sg_decrypt($userInfo->password,cfg('encrypt_key')))) return error(_HTTP_ERROR_NOT_ACCEPTABLE, 'รหัสผ่านปัจจุบันไม่ถูกต้อง');
 		else if (strlen($post->password) < 6) return error(_HTTP_ERROR_NOT_ACCEPTABLE, 'รหัสผ่านใหม่ต้องตัวอักษรอย่างน้อย 6 อักษร');
 		else if (strlen($post->password) != strlen($post->repassword) || $post->password != $post->repassword) return error(_HTTP_ERROR_NOT_ACCEPTABLE, 'การป้อนรหัสผ่านใหม่ทั้งสองครั้งไม่ตรงกัน');
 
 		UserModel::updatePassword(i()->uid, $post->password);
 
-		return success('New password was change : บันทึกรหัสผ่านใหม่เรียบร้อย'.mydb()->_query);
+		return success('New password was change : บันทึกรหัสผ่านใหม่เรียบร้อย');
 	}
 }
 ?>
