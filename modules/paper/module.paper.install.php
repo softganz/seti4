d44953f276a532cb55f52d245bc7c560bc447279
Little Bear <softganz@users.noreply.github.com>
2023-09-02 16:44:50 +0700

    Fixed bug: init object

--- modules/paper/module.paper.install.php
+++ modules/paper/module.paper.install.php
@@ -13,24 +13,29 @@ function module_paper_install() {
 
 	// create podcast content type
 	if (cfg('topic_options_page')==NULL) {
-		$topic_options->publish='publish';
-		$topic_options->promote=0;
-		$topic_options->comment=0;
+		$topic_options = (Object) [
+			'publish' => 'publish',
+			'promote' => 0,
+			'comment' => 0,
+		];
 		cfg_db('topic_options_page',$topic_options);
 	}
 
-	$stmt = 'INSERT IGNORE INTO %topic_types%
-					(`type`,`name`,`module`,`has_title`,`title_label`,`has_body`,`body_label`,`custom`,`modified`,`locked`)
-					VALUES (
-						"story","Story",NULL,1,"Topic",1,"Body",1,1,0
-					)';
-	mydb::query($stmt);
-	$queryResult[]=mydb()->_query;
+	mydb::query(
+		'INSERT IGNORE INTO %topic_types%
+		(`type`,`name`,`module`,`has_title`,`title_label`,`has_body`,`body_label`,`custom`,`modified`,`locked`)
+		VALUES (
+			"story","Story",NULL,1,"Topic",1,"Body",1,1,0
+		)'
+	);
+	$queryResult[] = mydb()->_query;
 
-	if (cfg('topic_options_story')==NULL) {
-		$topic_options->publish='publish';
-		$topic_options->promote=1;
-		$topic_options->comment=2;
+	if (cfg('topic_options_story') == NULL) {
+		$topic_options = (Object) [
+			'publish' => 'publish',
+			'promote' => 1,
+			'comment' => 2,
+		];
 		cfg_db('topic_options_story',$topic_options);
 	}
 
