9a7efe8e186654bea8acf8ff29dac3a059a603b4
Little Bear <softganz@users.noreply.github.com>
2023-11-18 11:16:13 +0700

    Feature: Show sticky node

--- modules/paper/default/page.paper.home.php
+++ modules/paper/default/page.paper.home.php
@@ -2,8 +2,8 @@
 /**
 * Paper   :: Home Page
 * Created :: 2019-01-01
-* Modify  :: 2023-07-26
-* Version :: 3
+* Modify  :: 2023-11-18
+* Version :: 4
 *
 * @return Widget
 *
@@ -41,6 +41,8 @@ class PaperHome extends Page {
 
 		// event_tricker('paper.listing.init',$self,$topics,$para);
 
+		$isFirstPage = empty($this->page) || $this->page == 1;
+
 		$topics = PaperModel::items([
 			'tags' => $this->tags,
 			'options' => [
@@ -75,52 +77,62 @@ class PaperHome extends Page {
 
 		// event_tricker('paper.listing.complete',$self,$topics,$para);
 
-		// // show topic that mark as home sticky
-		// if ($isFirstPage) {
-		// 	$sticky_para = (Object) [
-		// 		'sticky' => _HOME_STICKY,
-		// 		'option' => option('no_page_bottom,no_menu,no_header,no_package_footer,no_toolbar,no_div'),
-		// 		'options' => [
-		// 			'field' => 'detail,photo',
-		// 			'order' => 'weight',
-		// 			'sort' => 'asc',
-		// 			'limit' => 10,
-		// 		]
-		// 	];
-		// 	$sticky = PaperModel::items($sticky_para);
-		// }
+		// show topic that mark as home sticky
+		if ($isFirstPage) {
+			$sticky_para = (Object) [
+				'type' => '*',
+				'sticky' => _HOME_STICKY,
+				'option' => option('no_page_bottom,no_menu,no_header,no_package_footer,no_toolbar,no_div'),
+				'options' => [
+					'field' => 'detail,photo',
+					'order' => 'weight',
+					'sort' => 'asc',
+					'limit' => 10,
+				]
+			];
+			$sticky = PaperModel::items($sticky_para);
 
-		// if ($isFirstPage) {
-		// 	foreach ($promote->items as $key => $item) {
-		// 		if ($item->sticky==_HOME_STICKY) unset($promote->items[$key]);
-		// 	}
-		// }
+			foreach ($topics->items as $key => $item) {
+				if ($item->sticky==_HOME_STICKY) unset($promote->items[$key]);
+			}
+		}
 
 		return new Scaffold([
 			'appBar' => NULL, // AppBar
 			'body' => new Container([
 				'class' => 'tag-topics -style-'.$this->listStyle,
 				'children' => [
-					'<form class="search-box" method="get" action="'.url('paper/list').'" style="flex: 1">'
+					'<form class="search-box" method="get" action="'.url('paper/list').'">'
 					. '<input type="text" class="form-text -fill" name="q" value="'.htmlspecialchars(post('q')).'" placeholder="ป้อนหัวข้อที่ต้องการค้นหา" />'
 					// . '<button class="btn -link" type="submit" name="" value=""><i class="icon -material">search</i></button>'
 					. '</form>',
-					$pagenv->show,
-					new PaperListWidget([
+					$sticky->items ? new Container([
+						'id' => 'home-sticky',
+						'class' => 'home-sticky -sticky',
+						'child' => new PaperListWidget([
+							'children' => $sticky->items,
+						]),
+					]) : NULL, // Container
+					new Container([
 						'id' => 'home-promote',
-						'class' => 'promote',
-						'listStyle' => $this->listStyle,
-						'url' => q(),
-						'order' => $this->order,
-						'headerSortParameter' => ['listStyle' => $this->listStyle, 'page' => $this->page, 'items' => $this->items],
-						'children' => $topics->items,
-					]),
-					$pagenv->show,
-					// isset($GLOBALS['ad']->tags_list) ? new Container([
-					// 	'id' => 'ad-tags_list',
-					// 	'class' => 'ads',
-					// 	'child' => $GLOBALS['ad']->tags_list,
-					// ]) : NULL, // Container
+						'class' => 'home-promote -promote',
+						'children' => [
+							$pagenv->show,
+							new PaperListWidget([
+								'listStyle' => $this->listStyle,
+								'url' => q(),
+								'order' => $this->order,
+								'headerSortParameter' => ['listStyle' => $this->listStyle, 'page' => $this->page, 'items' => $this->items],
+								'children' => $topics->items,
+							]),
+							$pagenv->show,
+							// isset($GLOBALS['ad']->tags_list) ? new Container([
+							// 	'id' => 'ad-tags_list',
+							// 	'class' => 'ads',
+							// 	'child' => $GLOBALS['ad']->tags_list,
+							// ]) : NULL, // Container
+						], // children
+					]), // Container
 				], // children
 			]), // Container
 		]);
