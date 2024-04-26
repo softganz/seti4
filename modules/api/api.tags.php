5b31afbbcbd271d6711990a918f320e04b429ff3
Panumas <softganz@gmail.com>
2024-04-26 12:41:19 +0700

    feat: add attribute childOf on return

--- modules/api/api.tags.php
+++ modules/api/api.tags.php
@@ -2,25 +2,25 @@
 /**
 * API     :: Tags API
 * Created :: 2022-11-19
-* Modify  :: 2022-11-19
-* Version :: 2
+* Modify  :: 2024-04-26
+* Version :: 3
 *
-* @return Array
+* @return String/Array
 *
-* @usage api/tags/{vid}?input=tagString
+* @usage api/tags/{action}/{id}?input=tagString
 */
 
 use Softganz\DB;
 
 class TagsApi extends PageApi {
-	var $vid;
+	var $id;
 	var $input;
-	var $actionDefault = 'vid';
+	var $actionDefault = 'items';
 
-	function __construct($vid = NULL, $action = NULL) {
+	function __construct($action = NULL, $id = NULL) {
 		parent::__construct([
-			'action' => is_string($vid) ? $vid : $action,
-			'vid' => $vid,
+			'action' => $action,
+			'id' => SG\getFirstInt($id),
 			'input' => post('input'),
 		]);
 	}
@@ -28,34 +28,57 @@ class TagsApi extends PageApi {
 	function vid() {
 		if (!debug('process')) sendHeader('text/xml');
 		$len = strlen($this->input);
-		if (strpos($this->input,',')) {
-			$pre=substr($this->input,0,strrpos($this->input,','));
-			$this->input = trim(substr($this->input,strrpos($this->input,',')+1));
+		if (strpos($this->input, ',')) {
+			$pre = substr($this->input, 0, strrpos($this->input,','));
+			$this->input = trim(substr($this->input, strrpos($this->input,',')+1));
 		}
 
-		if ($len>0) {
-			$tags=mydb::select('SELECT tid,name FROM %tag% WHERE `vid`=:vid AND `name` LIKE :name ORDER BY `name` ASC',':vid',$this->vid,':name',$this->input.'%');
+		if ($len > 0) {
+			$tags = DB::select([
+				'SELECT tid,name
+				FROM %tag%
+				WHERE `vid` = :vid AND `name` LIKE :name
+				ORDER BY `name` ASC',
+				'var' => [
+					':vid' => $this->id,
+					':name' => $this->input.'%'
+				],
+			]);
 		}
 
-		$ret='<?xml version="1.0" encoding="utf-8" ?><results>';
+		$ret = '<?xml version="1.0" encoding="utf-8" ?><results>';
+
 		foreach ($tags->items as $tag) {
-			$ret.='<rs id="'.$tag->tid.'" info="">'.($pre?$pre.' , ':'').$tag->name.'</rs>';
+			$ret .= '<rs id="'.$tag->tid.'" info="">'.($pre?$pre.' , ':'').$tag->name.'</rs>';
 		}
-		//	$ret.='<rs id="99" info="Input text">'.$_GET['input'].' from '.$this->vid.'</rs>';
-		//	$ret.='<rs id="98" info="Input text">'.$this->.'</rs>';
-		//	$ret.='<rs id="97" info="Query">'.htmlspecialchars($tags->_query).'</rs>';
-			$ret.='</results>';
+
+		$ret .= '</results>';
 		die($ret);
+	}
+
+	function id() {
+		// debugMsg($this, '$this');
+		$tagId = SG\getFirstInt(post('id'));
+		if (empty($tagId)) return error(_HTTP_ERROR_BAD_REQUEST, 'ข้อมูลไม่ครบถ้วน');
+
 	}
 
 	function items() {
 		$childOf = post('childOf');
+
+		$result = (Object) [
+			'childOf' => NULL,
+			'count' => 0,
+			'items' => [],
+		];
 
 		$dbs = DB::select([
 			'SELECT
 			`tag`.`tid` `tagId`
 			, `tag`.`name`
 			, `hierarchy`.`parent`
+			, `tag`.`catId` `categoryId`
+			, `tag`.`catParent` `categoryParent`
 			FROM %tag% `tag`
 				LEFT JOIN %tag_hierarchy% `hierarchy` ON `tag`.`tid` = `hierarchy`.`tid`
 			%WHERE%',
@@ -66,10 +89,20 @@ class TagsApi extends PageApi {
 			],
 		]);
 
-		$result = (Object) [
-			'count' => count($dbs->items),
-			'items' => $dbs->items,
-		];
+		$result->count = count($dbs->items);
+		$result->items = $dbs->items;
+
+		// $result->query = mydb()->_query;
+
+		if ($childOf) {
+			$result->childOf = DB::select([
+				'SELECT `tid` `tagId`, `name` FROM %tag% WHERE `tid` = :tid LIMIT 1',
+				'var' => [':tid' => $childOf]
+			]);
+		} else {
+			unset($result->childOf);
+		}
+
 		return $result;
 	}
 }
