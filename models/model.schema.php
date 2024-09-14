1b0a42eb7291a1371cdc8aa9c0ec5ca33d6cf1dc
Panumas <softganz@gmail.com>
2024-09-14 17:12:44 +0700

    feat:add get only body input of schema

--- models/model.schema.php
+++ models/model.schema.php
@@ -1,8 +1,9 @@
 <?php
 /**
-* Schema :: Schema Model
-* Created 2022-09-23
-* Modify 	2022-09-23
+* Schema  :: Schema Model
+* Created :: 2022-09-23
+* Modify 	:: 2022-09-23
+* Version :: 2
 *
 * @param Array $args
 * @return Object
@@ -27,7 +28,7 @@ class SchemaModel {
 		return json_decode(R::Asset($schemaName));
 	}
 
-	public function indicator($section) {
+	public static function indicator($section) {
 		foreach ($this->body as $metrix) {
 			foreach ($metrix->items as $metrinItem) {
 				foreach ($metrinItem->indicator as $indicator) {
@@ -37,6 +38,91 @@ class SchemaModel {
 			}
 		}
 		return [];
+	}
+
+	public static function bodyOnly($body) {
+		$result = [];
+
+		foreach ($body as $key => $element) {
+			if (is_object($element)) {
+				// Create widget
+				if ($element->method) continue;
+				else if ($element->widget && $element->widget != 'ChildrenWidget') continue;
+				else if (in_array($element->type, ['textfield'])) continue;
+				else if ($element->widget === 'ChildrenWidget') {
+					$result = array_merge($result, SchemaModel::bodyOnly($element->children));
+				} else {
+					// debugMsg($element->inputName);
+					unset($element->options);
+					$result[] = $element;
+				}
+				// if ($element->widgetName) {
+				// 	$widgetName = $element->widget;
+				// 	unset($element->widget);
+				// 	if ($widgetName === "ChildrenWidget") {
+				// 		foreach ($element->children as $childrenKey => $childrenValue) {
+				// 			if (is_string($childrenValue) && preg_match('/^</', $childrenValue)) {
+				// 				$element->children[$childrenKey] = $childrenValue;
+				// 				continue;
+				// 			}
+				// 			$childrenValue = (Array) $childrenValue;
+				// 			if ($childrenValue['type'] === "widget") {
+				// 				$element->children[$childrenKey] = new $childrenValue['widget']((Array) $childrenValue);
+				// 			} else {
+				// 				$childrenValue['value'] = $this->data->{$childrenValue['inputName']};
+				// 				$element->children[$childrenKey] = (Array) $childrenValue;
+				// 			}
+				// 		}
+				// 	}
+				// 	$schema->body[$key] = new $widgetName((Array) $element);
+				// } else {
+				// 	unset($schema->body[$key]);
+				// }
+			} else if (is_string($element)) {
+				// unset($schema->body[$key]);
+			} else {
+			// } else if (is_array($element)) {
+			// 	// Get information from $qtCodes
+			// 	$elements = [];
+			// 	foreach ($element as $subKey => $elementId) {
+			// 		// Element start with html tag
+			// 		if (preg_match('/^</', $elementId)) {
+			// 			$elements[] = $elementId;
+			// 			continue;
+			// 		}
+
+			// 		$elementParam = SG\getFirst($qtCodes[$elementId]->detail, (Object) []);
+
+			// 		// Extract choice to array
+			// 		if ($elementParam->choices) {
+			// 			if (is_string($elementParam->choices)) {
+			// 				$choices = [];
+			// 				foreach (explode(',', $elementParam->choices) as $choiceText) {
+			// 					$choices[$choiceText] = $choiceText;
+			// 				}
+			// 			} else {
+			// 				$choices = $elementParam->choices;
+			// 			}
+			// 			$elementParam->options = (Array) $choices;
+			// 			unset($elementParam->choices);
+			// 		}
+			// 		// unset($elementParam->attribute);
+			// 		$elementParam->value = $this->data->{$elementId};
+
+			// 		$elements[$elementId] = (Array) $elementParam;
+			// 	}
+			// 	// debugMsg($elements, '$elements');
+			// 	$schema->body[$key] = new ChildrenWidget([
+			// 		'tagName' => 'div',
+			// 		'class' => 'widget-card personal-edit',
+			// 		'children' => $elements,
+			// 	]);
+			// } else {
+				$result[] = $element;
+				// debugMsg(htmlspecialchars($element));
+			}
+		}
+		return $result;
 	}
 }
 ?>
