dd6d55cd03b66433f68e9a3f364eaf0b504b8cec
Panumas <softganz@gmail.com>
2025-01-16 17:17:14 +0700

    refactor:change navigatr to Nav widget

--- modules/admin/widgets/widget.admin.app.bar.php
+++ modules/admin/widgets/widget.admin.app.bar.php
@@ -3,7 +3,7 @@
 * Admin   :: Admin AppBar Widget
 * Created :: 2024-08-19
 * Modify  :: 2025-01-16
-* Version :: 2
+* Version :: 3
 *
 * @param Array $_args
 * @return AppBar
@@ -43,32 +43,34 @@ class AdminAppBarWidget extends AppBar {
 	private function navigator() {
 		$activeUrl = q(1);
 
-		return array_merge(
-			[
-				is_admin() ? '<a href="'.url('admin').'"><i class="icon -material">home</i><span class="">Home</span></a>' : NULL,
-				//'<a href="'.url('admin/task').'">'.tr('By task').'</a>',
-				//'<a href="'.url('admin/module').'">'.tr('By modules').'</a>',
-				is_admin() ? '<a href="'.url('admin/content').'"><i class="icon -material">ballot</i><span>{tr:Content}</span></a>' : NULL,
-				is_admin() ? '<a href="'.url('admin/site').'"><i class="icon -material">build</i><span>Site</span></a>' : NULL,
-				user_access('administer users') ? '<a href="'.url('admin/user').'"><i class="icon -material">people</i><span>Users</span></a>' : NULL,
-				is_admin() ? '<a href="'.url('admin/config').'"><i class="icon -material">web</i><span>Config</span></a>' : NULL,
-				is_admin() ? '<a href="'.url('admin/log').'"><i class="icon -material">check_box</i><span>Logs</span></a>' : NULL
-			],
-			$activeUrl === 'site' ? [
-				'<hr>',
-				'<a href="'.url('admin/site/info').'"><i class="icon -material">ballot</i><span>{tr:Site Information}</span></a>',
-				'<a href="'.url('admin/site/theme').'"><i class="icon -material">ballot</i><span>{tr:Theme}</span></a>'
-			] : [],
-			$activeUrl === 'user' ? [
-				'<hr>',
-				'<a href="'.url('admin/user/list').'" title="All User"><i class="icon -material">ballot</i><span>All User</span></a>',
-				'<a href="'.url('admin/user/list','s=enable').'" title="Enabled User"><i class="icon -material">ballot</i><span>Enabled</span></a>',
-				'<a href="'.url('admin/user/list','s=disable').'" title="Disabled User"><i class="icon -material">ballot</i><span>Disabled</span></a>',
-				'<a href="'.url('admin/user/list','s=block').'" title="Blocked User"><i class="icon -material">ballot</i><span>Blocked</span></a>',
-				'<a href="'.url('admin/user/list','s=waiting').'" title="Waiting User"><i class="icon -material">ballot</i><span>Waiting</span></a>',
-				'<a href="'.url('admin/user/list','r=1').'" title="User have roles"><i class="icon -material">ballot</i><span>Roles</span></a>'
-			] : []
-		);
+		return new Nav([
+			'children' => array_merge(
+				[
+					is_admin() ? '<a href="'.url('admin').'"><i class="icon -material">home</i><span class="">Home</span></a>' : NULL,
+					//'<a href="'.url('admin/task').'">'.tr('By task').'</a>',
+					//'<a href="'.url('admin/module').'">'.tr('By modules').'</a>',
+					is_admin() ? '<a href="'.url('admin/content').'"><i class="icon -material">ballot</i><span>{tr:Content}</span></a>' : NULL,
+					is_admin() ? '<a href="'.url('admin/site').'"><i class="icon -material">build</i><span>Site</span></a>' : NULL,
+					user_access('administer users') ? '<a href="'.url('admin/user').'"><i class="icon -material">people</i><span>Users</span></a>' : NULL,
+					is_admin() ? '<a href="'.url('admin/config').'"><i class="icon -material">web</i><span>Config</span></a>' : NULL,
+					is_admin() ? '<a href="'.url('admin/log').'"><i class="icon -material">check_box</i><span>Logs</span></a>' : NULL
+				],
+				$activeUrl === 'site' ? [
+					'<sep>',
+					'<a href="'.url('admin/site/info').'"><i class="icon -material">ballot</i><span>{tr:Site Information}</span></a>',
+					'<a href="'.url('admin/site/theme').'"><i class="icon -material">ballot</i><span>{tr:Theme}</span></a>'
+				] : [],
+				$activeUrl === 'user' ? [
+					'<sep>',
+					'<a href="'.url('admin/user/list').'" title="All User"><i class="icon -material">ballot</i><span>All User</span></a>',
+					'<a href="'.url('admin/user/list','s=enable').'" title="Enabled User"><i class="icon -material">ballot</i><span>Enabled</span></a>',
+					'<a href="'.url('admin/user/list','s=disable').'" title="Disabled User"><i class="icon -material">ballot</i><span>Disabled</span></a>',
+					'<a href="'.url('admin/user/list','s=block').'" title="Blocked User"><i class="icon -material">ballot</i><span>Blocked</span></a>',
+					'<a href="'.url('admin/user/list','s=waiting').'" title="Waiting User"><i class="icon -material">ballot</i><span>Waiting</span></a>',
+					'<a href="'.url('admin/user/list','r=1').'" title="User have roles"><i class="icon -material">ballot</i><span>Roles</span></a>'
+				] : []
+			)
+		]);
 	}
 }
 ?>
