286afe4065fb1cae77d5387fb91d5d269a014797
Panumas <softganz@gmail.com>
2025-06-07 15:16:44 +0700

    refactor:Use DB

--- modules/system/page.system.issue.new.php
+++ modules/system/page.system.issue.new.php
@@ -2,7 +2,7 @@
 /**
 * System  :: Save New Issue
 * Created :: 2022-10-14
-* Modify  :: 2024-03-01
+* Modify  :: 2025-06-07
 * Version :: 3
 *
 * @return Widget
@@ -11,6 +11,8 @@
 *	Example call:
 * http://localhost/seti/softganz.com/system/issue/new?file=/Users/httpdocs/cms/seti4.00/modules/project/info/page.project.info.view.php&line=32&date=2022-10-14%2008:52:34user=&url=http://localhost/hsmi/localfund.com/project/141880&type=Test%20Log
 */
+
+use Softganz\DB;
 
 class SystemIssueNew extends Page {
 	var $reportUrl;
@@ -28,6 +30,7 @@ class SystemIssueNew extends Page {
 	var $description;
 
 	function __construct() {
+		// $post = json_decode(post());
 		$reportUrl = post('url');
 		list($scheme, $host) = $hosts = parse_url($reportUrl);
 
@@ -45,30 +48,47 @@ class SystemIssueNew extends Page {
 			'referer' => post('referer'),
 			'agent' => post('agent'),
 			'description' => post('description'),
+			'data' => json_encode(SG\getFirst(post('data'), (Object)[])),
 		]);
 	}
 
 	function build() {
-		mydb::query(
+		DB::query([
 			'INSERT INTO %system_issue%
 			(
-				`host`, `path`, `query`, `file`, `line`
+				`host`
+				, `path`
+				, `query`
+				, `file`
+				, `line`
 				, `issueType`
-				, `reportDate`, `reportUser`, `reportBy`
-				, `referer`, `agent`
+				, `reportDate`
+				, `reportUser`
+				, `reportBy`
+				, `referer`
+				, `agent`
 				, `description`
+				, `data`
 				, `created`
 			)
 			VALUES
 			(
-				:host, :path, :query, :file, :line
+				:host
+				, :path
+				, :query
+				, :file
+				, :line
 				, :issueType
-				, :reportDate, :reportUser, :reportBy
-				, :referer, :agent
+				, :reportDate
+				, :reportUser
+				, :reportBy
+				, :referer
+				, :agent
 				, :description
+				, :data
 				, :created
 			)',
-			[
+			'var' => [
 				':host' => $this->host,
 				':path' => $this->path,
 				':query' => $this->query,
@@ -81,16 +101,20 @@ class SystemIssueNew extends Page {
 				':referer' => $this->referer,
 				':agent' => $this->agent,
 				':description' => $this->description,
+				':data' => $this->data,
 				':created' => date('U'),
 			]
-		);
+		]);
 
-		// debugMsg(mydb()->_query);
+		// debugMsg(R()->_query);
 		// debugMsg($this, '$this');
 
 		return [
 			'issueId' => mydb()->insert_id,
 			'url' => $this->reportUrl,
+			// 'post' => post(),
+			// 'this'=> $this,
+			// 'query' => '<pre>'.R()->_query.'</pre>'
 		];
 	}
 }
