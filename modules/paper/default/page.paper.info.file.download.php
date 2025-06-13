f97960c6d2bbebb938892dda6fefa0ea9b0b6848
Panumas <softganz@gmail.com>
2025-06-13 18:34:09 +0700

    refactor:Change to pag widget

--- modules/paper/default/page.paper.info.file.download.php
+++ modules/paper/default/page.paper.info.file.download.php
@@ -1,69 +1,96 @@
 <?php
 /**
-* paper :: Show download file
-* Created 2021-01-06
-* Modify  2021-01-06
+* paper   :: Show download file
+* Created :: 2021-01-06
+* Modify  :: 2025-06-13
+* Version :: 2
 *
-* @param Object $self
-* @param Object $topicInfo
-* @return String
+* @param String $nodeInfo
+* @return Widget
 *
-* @usage paper/{id}/info.file
+* @usage paper/{nodeId}/info.file.download/{fileId}
 */
 
-$debug = true;
+use Softganz\DB;
 
-function paper_info_file_download($self, $topicInfo = NULL, $fid = NULL) {
-	// Data Model
-	$tpid = $topicInfo->tpid;
+class paperInfoFileDownload extends Page {
+	var $nodeId;
+	var $fileId;
+	var $nodeInfo;
 
-	if (!$tpid || !$fid) {
-		return new ErrorMessage([
-			'responseCode' => _HTTP_ERROR_NOT_FOUND,
-			'text' => 'ไม่พบไฟล์',
+	function __construct($nodeInfo = NULL, $fileId = NULL) {
+		parent::__construct([
+			'nodeId' => $nodeInfo->nodeId,
+			'fileId' => $fileId,
+			'nodeInfo' => $nodeInfo,
+			'fileId' => $fileId,
 		]);
 	}
 
-	$ret = '';
-
-	$fileInfo = mydb::select(
-		'SELECT f.*
-		FROM %topic_files% f
-		WHERE f.`tpid` = :tpid AND f.`type` = "doc" AND f.`tagname` IS NULL AND f.`fid` = :fid LIMIT 1',
-		[
-			':tpid' => $tpid,
-			':fid' => $fid,
-		]
-	);
-
-	if (!$fileInfo->count()) return message('error', 'ไม่พบไฟล์');
-
-	$stmt = 'UPDATE %topic_files% SET `download` = `download` + 1, `last_download` = :now
-		WHERE `fid` = :fid
-		LIMIT 1';
-
-	mydb::query($stmt, ':fid', $fid, ':now', date('U'));
-
-	// View Model
-	//$doc_file = cfg('paper.upload.document.folder').$doc->file;
-	$file = cfg('paper.upload.document.folder').$fileInfo->file;
-	//$ret .= $file;
-
-	if (file_exists($file) && is_file($file)) {
-		$ext = sg_file_extension($file);
-		header('Content-type: application/octet-stream');
-		header('Content-Disposition: attachment; filename="'.$fileInfo->title.'.'.$ext.'"');
-		readfile($file);
-		die;
-	} else {
-		$ret .= message('error','Download file '
-			. (user_access('access debugging program')?'('.$file.') ':'')
-			. 'not exists.');
+	function rightToBuild() {
+		if (empty($this->nodeId)) return error(_HTTP_ERROR_NOT_FOUND, 'ไม่มีหัวข้อ');
+		if (empty($this->fileId)) return error(_HTTP_ERROR_NOT_FOUND, 'ไม่มีไฟล์');
+		return true;
 	}
 
-	//$ret .= print_o($fileInfo);
+	function build() {
+		$ret = '';
 
-	//$ret .= print_o($topicInfo);
-	return $ret;
+		$fileInfo = $this->getFileInfo($this->nodeId, $this->fileId);
+
+		if (!$fileInfo->fileId) return message('error', 'ไม่พบไฟล์');
+
+		$this->increaseDownloadTime();
+
+		// View Model
+		//$doc_file = cfg('paper.upload.document.folder').$doc->file;
+		$file = cfg('paper.upload.document.folder').$fileInfo->file;
+		//$ret .= $file;
+
+		if (file_exists($file) && is_file($file)) {
+			$ext = sg_file_extension($file);
+			header('Content-type: application/octet-stream');
+			header('Content-Disposition: attachment; filename="'.$fileInfo->title.'.'.$ext.'"');
+			readfile($file);
+			die;
+		}
+		
+		$ret .= message('error','Download file '
+			. (user_access('access debugging program') ? '('.$file.') ' : '')
+			. 'not exists.');
+
+		return $ret;
+
+		
+		// return new Scaffold([
+		// 	'appBar' => new AppBar([
+		// 		'title' => 'Title',
+		// 	]), // AppBar
+		// 	'body' => new Widget([
+		// 		'children' => [], // children
+		// 	]), // Widget
+		// ]);
+	}
+
+	private function getFileInfo($nodeId, $fileId) {
+		return DB::select([
+			'SELECT f.`fid` `fileId`, f.*
+			FROM %topic_files% f
+			WHERE f.`tpid` = :tpid AND f.`type` = "doc" AND f.`tagname` IS NULL AND f.`fid` = :fid LIMIT 1',
+			'var' => [
+				':tpid' => $this->nodeId,
+				':fid' => $this->fileId,
+			]
+		]);
+	}
+
+	private function increaseDownloadTime() {
+		DB::query([
+			'UPDATE %topic_files% SET `download` = `download` + 1, `last_download` = :now
+			WHERE `fid` = :fid
+			LIMIT 1',
+			'var' => [':fid' => $this->fileId, ':now' => date('U')]
+		]);
+	}
 }
 ?>
--- modules/paper/default/page.paper.info.file.php
+++ modules/paper/default/page.paper.info.file.php
@@ -1,45 +1,80 @@
 <?php
 /**
-* paper :: Show download file
-* Created 2021-01-06
-* Modify  2021-01-06
+* paper   :: Show download file
+* Created :: 2021-01-06
+* Modify  :: 2025-06-13
+* Version :: 2
 *
-* @param Object $self
-* @param Object $topicInfo
-* @return String
+* @param String $nodeInfo
+* @return Widget
 *
-* @usage paper/{id}/info.file
+* @usage paper/{nodeId}/info.file
 */
 
-$debug = true;
+use Softganz\DB;
 
-function paper_info_file($self, $topicInfo) {
-	// Data Model
-	$tpid = $topicInfo->tpid;
+class paperInfoFile extends Page {
+	var $nodeId;
+	var $right;
+	var $nodeInfo;
 
-	$ret = '';
-
-	$stmt = 'SELECT f.*
-		FROM %topic_files% f
-		WHERE f.`tpid` = :tpid AND f.`type` = "doc" AND f.`tagname` IS NULL';
-
-	$dbs = mydb::select($stmt, ':tpid', $tpid);
-
-	// View Model
-	$tables = new Table();
-	foreach ($dbs->items as $rs) {
-		$downloadUrl = url('paper/'.$tpid.'/info.file.download/'.$rs->fid);
-		$tables->rows[] = array(
-			'<a href="'.$downloadUrl.'" target="_blank">'.$rs->title.'</a>',
-			'<a href="'.$downloadUrl.'" target="_blank"><i class="icon -material">cloud_download</i></a>',
-		);
+	function __construct($nodeInfo = NULL) {
+		parent::__construct([
+			'nodeId' => $nodeInfo->nodeId,
+			'nodeInfo' => $nodeInfo,
+			'right' => (Object) [
+				'edit' => $nodeInfo->right->edit
+			]
+		]);
 	}
 
-	$ret .= $tables->build();
-
-	//$ret .= print_o($dbs);
-
-	//$ret .= print_o($topicInfo);
-	return $ret;
+	function build() {
+		return new Scaffold([
+			'appBar' => new AppBar([
+				'title' => 'File Download',
+				'trailing' => new Row([
+					'child' => $this->right->edit ? new Button([
+						'class' => 'sg-action',
+						'href' => url('paper/'.$this->nodeId.'/edit.docs'),
+						'icon' => new Icon('edit'),
+						'rel' => 'box',
+						'attribute' => ['data-width' => 'full']
+					]) : NULL
+				]),
+			]), // AppBar
+			'body' => new Table([
+				'thead' => ['no' => '', 'ไฟล์', 'ext -center' => '', 'times -amt' => 'จำนวนดาวน์โหลด', 'last -date' => 'ล่าสุด', ''],
+				'children' => array_map(
+					function ($rs) {
+						static $no = 0;
+						$downloadUrl = url('paper/'.$rs->nodeId.'/info.file.download/'.$rs->fileId);
+						$ext = strtoupper(sg_file_extension($rs->file));
+						return [
+							++$no,
+							new Button([
+								'href' => $downloadUrl,
+								'text' => $rs->title,
+								'target' => '_blank'
+							]),
+							$ext,
+							number_format($rs->download),
+							$rs->last_download ? sg_date($rs->last_download, 'ว ดด ปปปป H:i') : '',
+							new Button([
+								'href' => $downloadUrl,
+								'icon' => new Icon('cloud_download'),
+								'target' => '_blank'
+							]),
+						];
+					},
+					(Array) DB::select([
+						'SELECT `file`.`tpid` `nodeId`, `file`.`title`, `file`.`download`, `file`.`last_download`, `file`.`fid` `fileId`, `file`.`file`
+							FROM %topic_files% `file`
+							WHERE `file`.`tpid` = :nodeId AND `file`.`type` = "doc" AND `file`.`tagname` IS NULL',
+							'var' => [':nodeId' => $this->nodeInfo->nodeId]
+					])->items
+				),
+			]), // Table
+		]);
+	}
 }
 ?>
