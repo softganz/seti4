2701cfc4bc2e23eff328710841aafa3afb6c4e18
Panumas <softganz@gmail.com>
2025-06-24 17:57:26 +0700

    fix:Change send mail to PHPMailer

--- modules/system/page.user.password.php
+++ modules/system/page.user.password.php
@@ -2,19 +2,29 @@
 /**
 * User    :: Reset Passsword Request Form
 * Created :: 2021-11-26
-* Modify  :: 2025-06-16
-* Version :: 3
+* Modify  :: 2025-06-24
+* Version :: 4
 *
 * @return Widget
 *
 * @usage user/password
 */
 
+use Softganz\DB;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+import('package:external/PHPMailer/src/PHPMailer.php');
+import('package:external/PHPMailer/src/SMTP.php');
+import('package:external/PHPMailer/src/Exception.php');
+
 class UserPassword extends Page {
+	var $host = 'mail.namfaa.com';
+
 	function build() {
 		$post = (Object) post('request');
 
-		if ($post->username) $result = $this->_sendMail($post->username);
+		if ($post->username) $result = $this->sendMail($post->username);
 
 		return new Scaffold([
 			'appBar' => new AppBar([
@@ -40,7 +50,7 @@ class UserPassword extends Page {
 							],
 							'send' => [
 								'type' => 'button',
-								'value' => '<i class="icon -material">email</i><span>Send password</span>',
+								'value' => '<i class="icon -material">email</i><span>Send reset password</span>',
 								'container' => '{class: "-sg-text-right"}',
 							],
 							$result ? '<div class="notify">'.$result.'</div>' : NULL,
@@ -52,7 +62,124 @@ class UserPassword extends Page {
 		]);
 	}
 
-	function _sendMail($username) {
+	// Generate uniq token
+	private static function token() {
+		do {
+			$token = md5(uniqid($username.rand(), true));
+			$tokenrs = DB::select([
+				'SELECT `code` FROM %users% WHERE `code` = :token LIMIT 1',
+				'var' => [':token' => $token]
+			]);
+		} while ($tokenrs->code);
+
+		return $token;
+	}
+
+	// Generate mail message
+	private static function mailMessage($username, $name, $email, $token) {
+		$resetUrl = _DOMAIN.url('user/resetpassword', ['sgpwcd' => $token]);
+
+		$message = 'สวัสดี คุณ '.$name
+			. '<p>คุณได้ร้องขอการยกเลิกรหัสผ่านของเว็บไซต์ '._DOMAIN.' โดยมีรายละเอียดผู้ใช้งานดังนี้<p>'
+			. '<p>user name : '.$username.'<br />'
+			. 'email address : '.$email.'</p>'
+			. '<p>กรุณา <a href="'.$resetUrl.'" target="_blank">คลิกลิงนี้</a> หรือ สำเนาที่อยู่ด้านล่างแล้วนำไปวางในช่อง address bar ของ browser และดำเนินการตามขั้นตอนด่อไป ลิงก์นี้จะสามารถใช้งานได้ภายใน 60 นาที<br><br>'.$resetUrl.'<br><br>กรุณาไม่ตอบกลับอีเมล์ฉบับนี้</p>'
+			. '<hr>'
+			. '<p>Password request from <strong>'._DOMAIN.'</strong> with detail below</p>'
+			. '<p>user name : '.$username.'<br />'
+			. 'email address : '.$email.'</p>'
+			. '<p>Please <a href="'.$resetUrl.'">click this link</a> or copy url below and paste into browser address bar</p> and following process. This link will expire within 60 minutes.<br /><br />'.$resetUrl.'</p>'
+			. '<p>Please do not reply this mail</p>';
+
+		return $message;
+	}
+
+	// Send email
+	private function sendMail($username) {
+		if (!load_lib('class.mail.php', 'lib')) return 'Mail module error';
+		$mail = new Mail();
+		if ($mail->IsValidEmail($username)) { // input email
+			$get_by_email = $username;
+		} else { // input username
+			$get_by_email = 'ตามที่ท่านได้ลงทะเบียนไว้ในชื่อ '.$username;
+		}
+
+		$users = DB::select([
+			'SELECT `uid`, `username`, `name`, `email` FROM %users% %WHERE%',
+			'where' => [
+				'%WHERE%' => [
+					$mail->IsValidEmail($username) ? ['`email` = :email', ':email' => $username] : ['`username` = :username', ':username' => $username],
+					['`status` = "enable"'],
+				]
+			]
+		]);
+
+		if (empty($users->items)) return 'ไม่มีข้อมูลสมาชิกตามรายละเอียดที่ท่านระบุ';
+
+		// debugMsg($users, '$users');
+
+		$cfgMail = cfg('mail');
+
+		list(,$mailDomain) = explode('@', $cfgMail->username);
+
+		foreach ($users->items as $user) {
+			$token = self::token();
+			$mail = new PHPMailer(true);
+			$logMsg = $username.' request new password.';
+
+			try {
+				//Server settings
+				$mail->CharSet = 'UTF-8';
+				$mail->isSMTP();
+				$mail->Host       = $cfgMail->host; // Set your SMTP server
+				$mail->SMTPAuth   = true;
+				$mail->Username   = $cfgMail->username; // SMTP username
+				$mail->Password   = $cfgMail->password; // SMTP password
+				$mail->SMTPSecure = $cfgMail->SMTPSecure;
+				$mail->Port       = $cfgMail->port;
+
+				//Recipients
+				$mail->setFrom('noreply@'.$mailDomain, 'noreply@'._DOMAIN_SHORT);
+				$mail->addAddress($user->email, $user->name);
+
+				// Content
+				$mail->isHTML(true);
+				$mail->Subject = 'ท่านร้องขอรหัสผ่านจาก '._DOMAIN_SHORT.' : Password request from '._DOMAIN_SHORT;
+				$mail->Body    = self::mailMessage($user->username, $user->name, $user->email, $token);
+				$mail->AltBody = 'This is the plain text body';
+
+				$mail->send();
+
+				$message= 'ได้ส่งข้อมูลไปให้ท่านที่อีเมล์ <strong>'.$get_by_email.'</strong> เรียบร้อยแล้ว :<br />กรุณาตรวจสอบอีเมล์ตามที่ท่านระบุไว้ในทะเบียนสมาชิก และคลิกลิงก์เพื่อเปลี่ยนรหัสผ่าน<br />หากยังไม่ได้รับอีเมล์ ให้ลองตรวจสอบใน <b>เมล์ขยะ (Spam mail)</b> ด้วยเพราะอาจจะถูกนำไปเก็บไว้ในนั้น';
+				$logMsg .= ' And password is send to email complete.';
+
+				DB::query([
+					'UPDATE %users% SET `code` = :token, `pwresettime` = :now WHERE `uid` = :uid LIMIT 1',
+					'var' => [
+						':token' => $token,
+						':now' => date('U'),
+						':uid' => $user->uid
+					]
+				]);
+			} catch (Exception $e) {
+				$message = "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+				$message = 'ท่านได้ร้องขอรหัสผ่านจากเว็บ แต่ท่านไม่ได้ระบุอีเมล์ไว้ในข้อมูลของสมาชิก จึงไม่สามารถส่งข้อมูลให้ท่านทางอีเมล์ได้.'.$mail->ErrorInfo;
+				$logMsg .= ' But email is invalid.';
+			}
+			// debugMsg($user, '$user');
+			// debugMsg($mail, '$mail');
+		}
+
+		LogModel::save([
+			'module' => 'user',
+			'keyword' => 'Password request',
+			'message' => $logMsg
+		]);
+
+		return $message;
+	}
+
+	private function sendMail_v1($username) {
 		if (!load_lib('class.mail.php', 'lib')) return 'Mail module error';
 
 		$logMsg = $username.' request new password.';
