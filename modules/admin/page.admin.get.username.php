066cb08834b3ed061532d1688886f88e9ef429ca
Panumas <softganz@gmail.com>
2025-07-14 10:42:06 +0700

    fix:autocomplete search user error

--- modules/admin/page.admin.get.username.php
+++ modules/admin/page.admin.get.username.php
@@ -1,6 +1,9 @@
 <?php
 /**
  * Search from username and name
+ * Crated  ::
+ * Mofidy  :: 2025-07-14
+ * Version :: 2
  * 
  * @param String $q or $_GET['q'] or $_POST['q'] : Substring in name
  * @param Integer $n or $_GET['n'] or $_POST['n'] : Item for result
@@ -15,25 +18,27 @@ function admin_get_username($self,$q='',$n=NULL,$p=NULL) {
 	$retType = SG\getFirst(post('r'),'u');
 	if (empty($q)) return '[]';
 	
-	$stmt='SELECT `uid`, `username`, `name`, `email`, `datein`
-					FROM %users%
-					WHERE `username` LIKE :q OR `name` LIKE :q OR `email` LIKE :q
-					ORDER BY `name` ASC
-					LIMIT '.($p-1).','.$n;
-	$dbs=mydb::select($stmt,':q','%'.$q.'%');
+	$dbs = mydb::select(
+		'SELECT `uid`, `username`, `name`, `email`, `datein`
+		FROM %users%
+		WHERE `username` LIKE :q OR `name` LIKE :q OR `email` LIKE :q
+		ORDER BY `name` ASC
+		LIMIT '.($p-1).','.$n,
+		':q','%'.$q.'%'
+	);
 	
 	$result=array();
 	foreach ($dbs->items as $rs) {
 		$result[] = array(
-									'value'=>$retType=='id'?$rs->uid:$rs->username,
-									'label'=>htmlspecialchars($rs->name),
-									'desc'=>$rs->username.' ('.$rs->uid.') '.$rs->email.' @'.sg_date($rs->datein,'d/m/Y')
-								);
+			'value' => $retType === 'id' ? $rs->uid : $rs->username,
+			'label' => htmlspecialchars($rs->name),
+			'desc' => $rs->username.' ('.$rs->uid.') '.$rs->email.' @'.sg_date($rs->datein,'d/m/Y')
+		);
 	}
 	if (debug('api')) {
 		$result[]=array('value'=>'query','label'=>$dbs->_query);
 		$result[]=array('value'=>'num_rows','label'=>'Result is '.$dbs->_num_rows.' row(s).');
 	}
-	return json_encode($result);
+	return $result;
 }
 ?>
