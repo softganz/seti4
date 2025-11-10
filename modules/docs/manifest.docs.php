4f35c3feb2182cb87fbea49e06a1340cf01db322
Panumas <softganz@gmail.com>
2025-11-10 13:48:25 +0700

    Fix: Add docs manifest file

---
+++ modules/docs/manifest.docs.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Docs class for documents management
+ *
+ * @package systedocs
+ * @version 1.00
+ * @copyright Copyright (c) 2000-present , The SoftGanz Group By Panumas Nontapan
+ * @author Panumas Nontapan <webmaster@softganz.com> , http://www.softganz.com
+ * @created 2025-11-10
+ * @modify 2025-11-10
+ * ============================================
+ * This program is free software. You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
+ * ============================================
+ */
+
+cfg('docs.version','1.00.00');
+cfg('docs.release','2025-11-10');
+
+menu('docs','Docs','docs','__controller',1,true,'static');
+?>
