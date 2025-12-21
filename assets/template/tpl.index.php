261375ccfbfe182896428e285d23f5f095afbcc8
Panumas <softganz@gmail.com>
2025-12-21 15:17:02 +0700

    Feat: Add auto dark mode from OS with themeModeFromOS core config

--- assets/template/tpl.index.php
+++ assets/template/tpl.index.php
@@ -190,6 +190,31 @@ window.cookieconsent.initialise({
 });
 </script>';
 ?>
+<?php if (cfg('system')->themeModeFromOS) {?>
+<script>
+//determines if the user has a set theme
+function detectColorScheme(){
+	let theme = "light";    //default to light
+
+	//local storage is used to override OS theme settings
+	if(localStorage.getItem("theme")){
+		if(localStorage.getItem("theme") == "dark") theme = "dark";
+	} else if(!window.matchMedia) {
+		//matchMedia method not supported
+		return false;
+	} else if(window.matchMedia("(prefers-color-scheme: dark)").matches) {
+		//OS theme setting detected as dark
+		theme = "dark";
+	}
+
+	//dark theme preferred, set document with a `data-theme` attribute
+	if (theme == "dark") {
+		document.body.classList.add("-app-theme-dark");
+	}
+}
+detectColorScheme();
+</script>
+<?php } ?>
 <?php echo cfg('mainScript');?>
 </body>
 </html>
