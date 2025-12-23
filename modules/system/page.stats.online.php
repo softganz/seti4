ec895cdbf57068a6b1592c9d8c3c2aad305e119a
Panumas <softganz@gmail.com>
2025-12-23 10:58:36 +0700

    Refactor: Use DB instate of mydb

--- modules/system/page.stats.online.php
+++ modules/system/page.stats.online.php
@@ -1,15 +1,18 @@
 <?php
 /**
-* Stats   :: Current User Online
-* Created :: 2018-09-01
-* Modify  :: 2024-08-30
-* Version :: 3
-*
-* @param String $arg1
-* @return Widget
-*
-* @usage stats/online
-*/
+ * Stats   :: Current User Online
+ * Author  :: Little Bear<softganz@gmail.com>
+ * Created :: 2018-09-01
+ * Modify  :: 2025-12-23
+ * Version :: 4
+ *
+ * @param String $arg1
+ * @return Widget
+ *
+ * @usage stats/online
+ */
+
+use Softganz\DB;
 
 class StatsOnline extends Page {
 	var $onlineType;
@@ -25,17 +28,21 @@ class StatsOnline extends Page {
 	function build() {
 		$today = today();
 
-		$onlines = mydb::select('SELECT COUNT(*) `total` FROM %users_online% LIMIT 1')->total;
+		$onlines = DB::select(['SELECT COUNT(*) `total` FROM %users_online% LIMIT 1'])->total;
 
 		$yesterday = date('Y-m-d',mktime(0,0,0,$today->mon,$today->mday-1,$today->year));
 
-		mydb::where('log_date>= :yesterday', ':yesterday', $yesterday);
-		$rs = mydb::select(
+		$rs = DB::select([
 			'SELECT `log_date`, `hits`, `users`
 			FROM %counter_day%
 			%WHERE%
-			ORDER BY log_date DESC LIMIT 2'
-		);
+			ORDER BY log_date DESC LIMIT 2',
+			'where' => [
+				'%WHERE%' => [
+					['log_date>= :yesterday', ':yesterday' => $yesterday]
+				],
+			],
+		]);
 
 		foreach ($rs->items as $item) {
 			if ($item->log_date == $today->date) $today_hits = $item;
@@ -54,10 +61,11 @@ class StatsOnline extends Page {
 					'<a href="'.url('stats/online', ['show' => 'bot']).'"><i class="icon -material">block</i><span>BOT</span></a>',
 				], // Navigator
 			]), // AppBar
-			'body' => new Widget([
+			'body' => new Card([
 				'children' => [
 					new ListTile(['title' => 'Members Online', 'leading' => new Icon('groups')]),
 					new Container([
+						'class' => '-sg-paddingnorm',
 						'children' => [
 							'Today <strong>'.number_format($today_hits->hits).'</strong> hits from <strong>'.number_format($today_hits->users).'</strong> users. ',
 							'Yesterday <strong>'.number_format($yesterday_hits->hits).'</strong> hits from <strong>'.number_format($yesterday_hits->users).'</strong> users.'
@@ -88,12 +96,11 @@ class StatsOnline extends Page {
 									($this->right->fullView ? '<a href="'.url('stats/list',array('ip'=>$full_ip)).'">' : '')
 									. $show_ip
 									. ($this->right->fullView ? '</a>' : ''),
-									// . ($full_ip === GetEnv('REMOTE_ADDR') ? '<i class="icon -material">person</i>' : ''),
 									$rs->username ? '<img class="profile-photo -sg-24" src="'.BasicModel::user_photo($rs->username).'" />' : '',
 									$this->right->fullView && $rs->name ? '<a href="'.url('stats/list',array('user'=>$rs->uid)).'">'.$rs->name.'</a>' : $rs->name,
 									date(($current_date != date('Y-m-d',$rs->coming) ? 'Y-m-d ' : '').'H:i:s',$rs->coming),
 									date(($current_date != date('Y-m-d',$rs->access) ? 'Y-m-d ' : '').'H:i:s',$rs->access),
-									$rs->hits,
+									number_format($rs->hits),
 									$rs->browser.' '.($this->right->fullView ? $rs->host : ''),
 									$this->right->fullView ? '<a class="sg-action btn -link" href="'.url('admin/ban/request', ['ip' => $full_ip, 'host' => $rs->host]).'" data-rel="box" data-width="800"><i class="icon -material">block</i></a>' : '',
 								];
