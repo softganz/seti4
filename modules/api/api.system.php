c996069b3c10da6fb262fcd58ed7bb507e27d6df
Panumas <softganz@gmail.com>
2026-03-14 11:59:23 +0700

    Feat: Add info API to system

--- modules/api/api.system.php
+++ modules/api/api.system.php
@@ -2,8 +2,8 @@
 /**
 * System  :: System API
 * Created :: 2022-10-14
-* Modify  :: 2025-02-25
-* Version :: 3
+* Modify  :: 2025-03-14
+* Version :: 4
 *
 * @param Int $mainId
 * @param String $action
@@ -31,10 +31,21 @@ class SystemApi extends PageApi {
 	}
 
 	private function _accessDenied() {
-		return [
-			'responseCode' => _HTTP_ERROR_FORBIDDEN,
-			'text' => 'Access Denied',
-		];
+		return apiError(_HTTP_ERROR_FORBIDDEN, 'Access Denied');
+	}
+
+	function info() {
+		header('Access-Control-Allow-Origin: *');
+		return apiSuccess([
+			'coreName' => 'Seti',
+			'coreVersion' => cfg('core.version'),
+			'databaseVersion' => cfg('version.install'),
+			'date' => date('Y-m-d'),
+			'time' => date('H:i:s'),
+			'online' => CounterModel::onlineMemberCount(),
+			'member' => CounterModel::onlineCount(),
+			'ip' => $_SERVER['REMOTE_ADDR'],
+		]);
 	}
 
 	function date() {
