880888d3598f4b80f80fc958f0bf9a2e622fc2d1
Panumas <softganz@gmail.com>
2026-03-16 15:40:35 +0700

    Refactor: Change from mydb to DB

--- model/model.reaction.php
+++ model/model.reaction.php
@@ -1,14 +1,16 @@
 <?php
 /**
-* Reaction Model :: Reaction Model
-* Created 2021-09-29
-* Modify 	2021-09-29
-*
-* @usage new ReactionModel([])
-* @usage ReactionModel::function($conditions, $options)
-*/
+ * Reaction:: Reaction Model
+ * Author  :: Little Bear<softganz@gmail.com>
+ * Created :: 2021-09-29
+ * Modify  :: 2026-03-16
+ * Version :: 2
+ *
+ * @usage new ReactionModel([])
+ * @usage ReactionModel::function($conditions, $options)
+ */
 
-$debug = true;
+use Softganz\DB;
 
 class ReactionModel {
 
@@ -29,7 +31,7 @@ class ReactionModel {
 
 		if (!$conditions->id) return NULL;
 
-		$result = mydb::select(
+		$result = DB::select([
 			'SELECT
 			`tpid` `topicId`, `approve`
 			, `rating`, `rateTimes`, `likeTimes`
@@ -37,18 +39,18 @@ class ReactionModel {
 			FROM %topic%
 			WHERE `tpid` = :topicId
 			LIMIT 1',
-			[':topicId' => $conditions->id]
-		);
+			'var' => [':topicId' => $conditions->id]
+		]);
 
 		$result = mydb::clearProp($result);
 
 		if ($conditions->bookmark) {
-			$result->bookmarks = mydb::select(
+			$result->bookmarks = DB::select([
 				'SELECT COUNT(*) `totals`
 				FROM %reaction%
 				WHERE `refid` = :topicId AND `action` = :action LIMIT 1',
-				[':topicId' => $conditions->id, ':action' => $conditions->bookmark]
-			)->totals;
+				'var' => [':topicId' => $conditions->id, ':action' => $conditions->bookmark]
+			])->totals;
 		}
 
 		return $result;
@@ -73,18 +75,16 @@ class ReactionModel {
 
 		$result = (Object) ['count' => 0, 'items' => []];
 
-		if ($conditions->id) mydb::where('r.`refid` = :refId', ':refId', $conditions->id);
-		if ($conditions->userId) mydb::where('r.`uid` = :userId', ':userId', $conditions->userId);
-		if ($conditions->action) mydb::where('r.`action` LIKE :action', ':action', $conditions->action);
-
-		mydb::value('$LIMIT$', '');
-
-		$result->items = mydb::select(
+		$result->items = DB::select([
 			'SELECT *, FROM_UNIXTIME(`dateact`, "%Y-%m-%d %H:%i:%s") `dateact`
 			FROM %reaction% r
-			%WHERE%
-			$LIMIT$'
-		)->items;
+			%WHERE%',
+			'%WHERE%' => [
+				$conditions->id ? ['r.`refid` = :refId', ':refId' => $conditions->id] : NULL,
+				$conditions->userId ? ['r.`uid` = :userId', ':userId' => $conditions->userId] : NULL,
+				$conditions->action ? ['r.`action` LIKE :action', ':action' => $conditions->action] : NULL,
+			],
+		])->items;
 
 		$result->count = count($result->items);
 
@@ -101,11 +101,12 @@ class ReactionModel {
 		//debugMsg('refid = '.$refid.' , action = '.$action);
 		// Add reaction to table reaction
 		if (i()->ok) {
-			$data = new stdClass();
-			$data->refid = $refid;
-			$data->uid = i()->uid;
-			$data->action = $action;
-			$data->dateact = date('U');
+			$data = (Object) [
+				'refid' => $refid,
+				'uid' => i()->uid,
+				'action' => $action,
+				'dateact' => date('U')
+			];
 
 			$stmt = 'INSERT INTO %reaction%
 				(`refid`,`uid`,`action`,`dateact`)
@@ -113,10 +114,16 @@ class ReactionModel {
 				(:refid,:uid,:action,:dateact)';
 
 			if ($options->addType == 'toggle') {
-				$hasReaction = mydb::select('SELECT `actid` FROM %reaction% WHERE `refid` = :refid AND `uid` = :uid AND `action` = :action LIMIT 1',$data)->actid;
+				$hasReaction = DB::select([
+					'SELECT `actid` FROM %reaction% WHERE `refid` = :refid AND `uid` = :uid AND `action` = :action LIMIT 1',
+					'var' => $data
+				])->actid;
 				//debugMsg($hasReaction. mydb()->_query);
 				if ($hasReaction) {
-					mydb::query('DELETE FROM %reaction% WHERE `actid` = :actid LIMIT 1', ':actid', $hasReaction);
+					DB::query([
+						'DELETE FROM %reaction% WHERE `actid` = :actid LIMIT 1',
+						'var' => [':actid' => $hasReaction]
+					]);
 					$result = false;
 					//debugMsg(mydb()->_query);
 					unset($stmt);
@@ -125,43 +132,58 @@ class ReactionModel {
 				}
 			}
 
-
-			if ($stmt) mydb::query($stmt,$data);
-
-			if (mydb()->_error) {
-				$createStmt = 'CREATE TABLE %reaction% (
-					`actid` int(11) NOT NULL AUTO_INCREMENT,
-					`refid` int(11) NULL,
-					`uid` int(11) NULL,
-					`action` varchar(10) NULL,
-					`dateact` bigint(20) NULL,
-					PRIMARY KEY (`actid`),
-					KEY `refid` (`refid`),
-					KEY `uid` (`uid`),
-					KEY `action` (`action`),
-					KEY `dateact` (`dateact`)
-				);';
-				mydb::query($createStmt);
+			try {
+				if ($stmt) DB::query([
+					$stmt,
+					'var' => $data
+				]);
+			} catch (Exception $exception) {
+				DB::query([
+					'CREATE TABLE %reaction% (
+						`actid` int(11) NOT NULL AUTO_INCREMENT,
+						`refid` int(11) NULL,
+						`uid` int(11) NULL,
+						`action` varchar(10) NULL,
+						`dateact` bigint(20) NULL,
+						PRIMARY KEY (`actid`),
+						KEY `refid` (`refid`),
+						KEY `uid` (`uid`),
+						KEY `action` (`action`),
+						KEY `dateact` (`dateact`)
+					);'
+				]);
 				// Requery add
-				mydb::query($stmt, $data);
+				DB::query([
+					$stmt,
+					'var' => $data
+				]);
 				$result = true;
 			}
 
 			if ($options->count == 'topic:liketimes') {
-				$stmt = 'UPDATE %topic% SET `liketimes` = IF(`liketimes` >= 0, `liketimes` '.($hasReaction ? '-':'+').' 1, 0) WHERE `tpid` = :tpid LIMIT 1';
-				mydb::query($stmt, ':tpid',$refid);
+				DB::query([
+					'UPDATE %topic% SET `liketimes` = IF(`liketimes` >= 0, `liketimes` '.($hasReaction ? '-':'+').' 1, 0) WHERE `tpid` = :tpid LIMIT 1',
+					'var' => [':tpid' => $refid]
+				]);
 				//debugMsg(mydb()->_query);
 			} else if ($options->count == 'msg:liketimes') {
-				$stmt = 'UPDATE %msg% SET `liketimes` = IF(`liketimes` >= 0, `liketimes` '.($hasReaction ? '-':'+').' 1, 0) WHERE `msgid` = :msgid LIMIT 1';
-				mydb::query($stmt, ':msgid',$refid);
-				if ($debug) debugMsg(mydb()->_query);
+				DB::query([
+					$stmt = 'UPDATE %msg% SET `liketimes` = IF(`liketimes` >= 0, `liketimes` '.($hasReaction ? '-':'+').' 1, 0) WHERE `msgid` = :msgid LIMIT 1',
+					'var' => [':msgid' => $refid]
+				]);
+				if ($debug) debugMsg(R('query'));
 			}
 		}
 
 		// Update topic view count
 		if ($action == 'TOPIC.VIEW' && $options->updateView) {
-			$stmt = 'UPDATE %topic% SET `view` = `view` + 1, `last_view` = :last_view WHERE `tpid` = :tpid LIMIT 1';
-			mydb::query($stmt,':tpid',$refid, ':last_view',date("Y-m-d H:i:s"));
+			DB::query([
+				'UPDATE %topic% SET `view` = `view` + 1, `last_view` = :last_view WHERE `tpid` = :tpid LIMIT 1',
+				'var' => [
+					':tpid' => $refid,
+					':last_view' => date("Y-m-d H:i:s")
+				]
+			]);
 		}
 		return $result;
 	}
