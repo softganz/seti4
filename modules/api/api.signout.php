559e4a3bfd8db06cfa5335ddbab96d506c7aa70d
Panumas <softganz@gmail.com>
2026-04-05 18:19:18 +0700

    Refactor: Add comment

--- modules/api/api.signout.php
+++ modules/api/api.signout.php
@@ -1,16 +1,15 @@
 <?php
 /**
-* API     :: User Sign Out
-* Created :: 2022-07-11
-* Modify  :: 2022-11-19
-* Version :: 2
-*
-* @return JSON
-*
-* @usage api/signout
-*/
-
-import('model:user.php');
+ * API     :: User Sign Out
+ * Author  :: Little Bear<softganz@gmail.com>
+ * Created :: 2022-07-11
+ * Modify  :: 2026-04-05
+ * Version :: 3
+ *
+ * @return JSON
+ *
+ * @usage api/signout
+ */
 
 class SignoutApi extends PageApi {
 	function build() {
