6ad03f6e07f6ea6b1d26f4b0040057dfe5e9b1e0
Panumas <softganz@gmail.com>
2026-04-29 18:59:11 +0700

    Refactor: Add class name

--- widget/widget.statusbar.php
+++ widget/widget.statusbar.php
@@ -1,28 +1,24 @@
 <?php
 /**
-* Status  :: Widget
-* Created :: 2025-06-16
-* Modify  :: 2025-06-16
-* Version :: 1
-*
-* @param Array $args
-* @return Object
-*
-* @usage import('widget:module.widgetlname.php')
-* @usage new StatusBarWidgetl([])
-*/
+ * Status  :: Widget
+ * Created :: 2025-06-16
+ * Modify  :: 2025-06-16
+ * Version :: 1
+ *
+ * @param Array $args
+ * @return Object
+ *
+ * @usage import('widget:module.widgetlname.php')
+ * @usage new StatusBarWidgetl([])
+ */
 
 class StatusbarWidget extends Widget {
-	var $childTagName = 'li';
+	var $tagName = 'ul';
+	var $childContainer = ['tagName' => 'li', 'class' => '-item'];
+	var $class = 'widget-statusbar';
 
-	function __construct($args = []) {
-		parent::__construct($args);
-	}
-
-	// function build() {
-	// 	return new Widget([
-	// 		'children' => $this->children
-	// 	]);
+	// function __construct($args = []) {
+	// 	parent::__construct($args);
 	// }
 }
 ?>
