0defbe676b2c0fc8192a86a416256256d0bc6735
Panumas <softganz@gmail.com>
2026-05-20 18:00:22 +0700

    Refactor: Change to DB

--- modules/api/api.ampur.php
+++ modules/api/api.ampur.php
@@ -3,55 +3,74 @@
  * API     :: Ampur API
  * Author  :: Little Bear<softganz@gmail.com>
  * Created :: 2022-11-19
- * Modify  :: 2026-02-24
- * Version :: 3
+ * Modify  :: 2026-05-20
+ * Version :: 4
  *
  * @return Array
  *
  * @usage api/ampur?changwat=changwatId
  */
 
+use Softganz\DB, Softganz\SetDataModel;
+
 class AmpurApi extends PageApi {
-	var $changwat;
+	var $changwatId;
+	var $changwatName;
+	var $group;
 
 	function __construct() {
 		parent::__construct([
-			'changwat' => \SG\getFirst(post('changwat'), post('q')),
+			'changwatName' => Request::get(['changwat', 'q']),
+			'changwatId' => Request::get(['changwat', 'q'], '/^[\d\,]+$/'),
+			'group' => Request::get('group', 'en'),
 		]);
 	}
 
 	function build() {
 		$result = [];
 
-		if (empty($this->changwat)) return $result;
+		if (empty($this->changwatId) && empty($this->changwatName)) return $result;
 
-		$dbs = mydb::select(
-			'SELECT `distid`, `distname`, `provname`
-				FROM %co_district% co
-					LEFT JOIN %co_province% cop ON cop.`provid` = LEFT(co.`distid`,2)
-				WHERE
-				'.(is_numeric($this->changwat) ? 'cop.`provid` = :provid' : '`distname` LIKE :q OR `provname` LIKE :q').'
-					AND RIGHT(`distname`,1) != "*"
-				ORDER BY CONVERT(`distname` USING tis620) ASC',
-			[
-				':q' => '%'.$this->changwat.'%',
-				':provid' => $this->changwat
-			]
-		);
+		$dbs = DB::select([
+			'SELECT `distId` AS `ampurCode`, `distName` AS `ampurName`
+			, `provId` AS `changwatCode`, `provName` AS `changwatName`
+			FROM %co_district% AS `cod`
+				LEFT JOIN %co_province% AS `cop` ON cop.`provId` = LEFT(`cod`.`distId`, 2)
+			%WHERE%
+			ORDER BY CONVERT(`ampurName` USING tis620) ASC',
+			'%WHERE%' => [
+				['RIGHT(`distname`, 1) != "*"'],
+				$this->changwatId ? ['`cop`.`provId` IN ( :changwatId )'] : ['`distname` LIKE :changwatName OR `provName` LIKE :changwatName'],
+			],
+			'var' => [
+				':changwatName' => '%' . $this->changwatName . '%',
+				':changwatId' => new SetDataModel($this->changwatId)
+			],
+		]);
 
 		foreach ($dbs->items as $rs) {
-			$label = is_numeric($this->changwat) ? htmlspecialchars($rs->distname) : htmlspecialchars(' อำเภอ'.$rs->distname.' จังหวัด'.$rs->provname);
+			$label = is_numeric($this->changwatCode) ? htmlspecialchars($rs->ampurName) : htmlspecialchars(' อำเภอ'.$rs->ampurName.' จังหวัด'.$rs->changwatName);
 
-			$result[] = [
-				'value' => $rs->distid,
-				'ampur' => substr($rs->distid, -2),
-				'label' => $label
+			$value = [
+				'value' => $rs->ampurCode,
+				'ampur' => substr($rs->ampurCode, -2),
+				'label' => $label,
+				'ampurCode' => $rs->ampurCode,
+				'ampurName' => $label,
+				'changwatCode' => $rs->changwatCode,
+				'changwatName' => $rs->changwatName,
 			];
+
+			if ($this->group) {
+					$result[$value['changwatCode']][$value['ampur']] = $value;
+			} else {
+				$result[] = $value;
+			}
 		}
 
 		if (debug('api')) {
-			$result[] = ['value' => 'query','label' => mydb()->_query];
-			$result[] = ['value' => 'num_rows','label' => 'Result is '.$dbs->_num_rows.' row(s).'];
+			$result[] = ['value' => 'query','label' => R('query')];
+			$result[] = ['value' => 'num_rows','label' => 'Result is ' . count((Array) $dbs->items) . ' row(s).'];
 		}
 
 		return $result;
