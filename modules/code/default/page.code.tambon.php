5f72354467223264539d13d8929cbf5d081a8c9c
Panumas <softganz@gmail.com>
2026-08-23 18:00:44 +0700

    refactor: change mydb query to DB query

--- modules/code/default/page.code.tambon.php
+++ modules/code/default/page.code.tambon.php
@@ -1,31 +1,34 @@
 <?php
 /**
-* Tambon Code
-* Created 2020-01-29
-* Modify  2020-01-29
-*
-* @param Object $self
-* @param Int $ampurId
-* @return String
-*/
+ * Code     :: Tambon Code
+ * Author   :: Little Bear<softganz@gmail.com>
+ * Created  :: 2020-01-29
+ * Modified :: 2026-08-23
+ * Version  :: 2
+ *
+ * @param Object $self
+ * @param Int $ampurId
+ * @return String
+ */
 
-$debug = true;
+use Softganz\DB;
 
 function code_tambon($self, $ampurId = NULL) {
 	$ret = '<header class="header">'._HEADER_BACK.'<h3>รหัสตำบล</h3></header>';
 
 	if (empty($ampurId)) return message('error','กรุณาระบุรหัสตำบล');
 
-	if ($ampurId) mydb::where('LEFT(`subdistid`,4) = :ampurId', ':ampurId', $ampurId);
-
-	$stmt = 'SELECT
+	$dbs = DB::select([
+		'SELECT
 		d.*
 		, (SELECT COUNT(*) FROM %co_village% WHERE LEFT(`villid`,6) = `subdistid`) `totalVillage`
 		FROM %co_subdistrict% d
 		%WHERE%
-		ORDER BY `subdistid` ASC';
-
-	$dbs = mydb::select($stmt);
+		ORDER BY `subdistid` ASC',
+		'%WHERE%' => [
+			$ampurId ? ['LEFT(`subdistid`,4) = :ampurId', ':ampurId' => $ampurId] : null
+		]
+	]);
 
 	$tables = new Table();
 	$tables->thead = array(
