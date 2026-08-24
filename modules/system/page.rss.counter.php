711dc1f66107298a7d9e933c2f096d837e28df46
Panumas <softganz@gmail.com>
2026-08-24 15:06:39 +0700

    refactor: change mydb query to DB query

--- modules/system/page.rss.counter.php
+++ modules/system/page.rss.counter.php
@@ -1,34 +1,36 @@
 <?php
 /**
-* RSS :: Get Counter
-* Created 2021-09-20
-* Modify  2022-05-03
-*
-* @param String $arg1
-* @return Widget
-*
-* @usage rss/counter
-*/
+ * RSS      :: Get Counter
+ * Author   :: Little Bear<softganz@gmail.com>
+ * Created  :: 2021-09-20
+ * Modified :: 2026-08-24
+ * Version  :: 2
+ *
+ * @return Widget
+ *
+ * @uses rss/counter
+ */
+
+use Softganz\DB;
 
 class RssCounter extends Page {
 	function build() {
-		// sendheader($type = 'text/xml');
 		$timer = new Timer();
 		$timer->start(0);
 
-		$day = \SG\getFirst($para->day, 7);
+		$day = (int) \SG\getFirst(Request::all('day', 'int'), 7);
 
 		$counter = cfg('counter');
 
-		mydb::value('$LIMIT$', $day);
-		$result = mydb::select(
+		$result = DB::select([
 			'SELECT
 				`log_date`, SUM(`hits`) `hits`, SUM(`users`) `users`
 			FROM %counter_day%
 			GROUP BY `log_date`
 			ORDER BY `log_date` DESC
-			LIMIT $LIMIT$'
-		);
+			LIMIT $LIMIT$',
+			'var' => ['$LIMIT$' => $day]
+		]);
 
 		$timer->stop(0);
 
@@ -63,51 +65,6 @@ class RssCounter extends Page {
 		}
 
 		return $channel;
-
-		// $ret .= $this->_create_rss($channel);
-
-		// return $ret;
-	}
-
-	/*
-	 * Create rss feed from array
-	 *
-	 * @param Array $channel
-	 * @return String
-	 */
-	function _create_rss($channel=array()) {
-		$header='';
-		$items='';
-		foreach ($channel as $key=>$value) {
-			if ($key=='item' && is_array($value)) {
-				foreach ($value as $item) {
-					$items.='<item>'._NL;
-					foreach ($item as $ikey=>$ivalue) {
-						if (is_array($ivalue)) { // This item is array
-							$items.='	<'.$ikey.' ';
-							$item_value=NULL;
-							foreach ($ivalue as $k1=>$v1) {
-								if (is_string($k1)) $items.=$k1.'="'.$v1.'" ';
-								else $item_value=$v1;
-							}
-							$items.=isset($item_value) ? '>'.$item_value.'</'.$ikey.'>':'/>';
-							$items.=_NL;
-						} else if (is_string($ivalue) || is_numeric($ivalue)) { // This item is string
-							$items.='	<'.$ikey.'>'.$ivalue.'</'.$ikey.'>'._NL;
-						}
-					}
-					$items.='</item>'._NL;
-				}
-			} else if (is_string($value)) {	// This item is in header
-				$header.='<'.$key.'>'.htmlspecialchars($value).'</'.$key.'>'._NL;
-			}
-		}
-		$ret='<?xml version="1.0" encoding="'.cfg('client.characterset').'"?>
-<rss version="2.0" xml:base="'.cfg('domain').'"  xmlns:dc="http://purl.org/dc/elements/1.1/">
-<channel>
-'.$header.$items.'</channel>
-</rss>';
-		return $ret;
 	}
 }
 ?>
