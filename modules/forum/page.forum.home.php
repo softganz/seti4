08e3f370128b6453ed2908269ef454bf41c4b028
Panumas <softganz@gmail.com>
2026-08-24 15:17:49 +0700

    refactor: change mydb query to DB query

--- modules/forum/page.forum.home.php
+++ modules/forum/page.forum.home.php
@@ -1,17 +1,19 @@
 <?php
 /**
-* Forum   :: Home
-* Created :: 2020-04-03
-* Modify  :: 2023-07-26
-* Version :: 2
-*
-* @param Object $self
-* @param Int $forumId
-* @return String
-*/
+ * Forum    :: Home
+ * Author   :: Little Bear<softganz@gmail.com>
+ * Created  :: 2020-04-03
+ * Modified :: 2026-08-24
+ * Version  :: 3
+ *
+ * @param Object $self
+ * @param Int $forumId
+ * @return String
+ */
 
 use Paper\Model\PaperModel;
 use Paper\Widget\PaperListWidget;
+use Softganz\DB;
 
 function forum_home($self, $forumId = NULL) {
 	$defaultItems = 50;
@@ -72,66 +74,65 @@ function forum_home($self, $forumId = NULL) {
 
 	//content('type','forum');
 
-	if (user_access('create forum content')) {
-		$ret .= '<nav class="nav -page -sg-text-right"><a class="btn -primary" href="'.url('paper/post/forum').'"><i class="icon -material">add_circle</i><span>{tr:Post new Forum topic}</span></a></nav>';
-	}
 
+	$vocab = DB::select([
+		'SELECT * FROM %vocabulary_types% WHERE `type` = "forum" LIMIT 1'
+	]);
 
-	//$ret .= print_o($topicOptions, '$topicOptions');
-	//$ret .= print_o($topicCondition, '$topicCondition');
-
-
-	$vocab = mydb::select('SELECT * FROM %vocabulary_types% WHERE type="forum" LIMIT 1');
-
-	if ($vocab->_num_rows) {
+	if ($vocab->_nuvidm_rows) {
 		$tree = BasicModel::get_taxonomy_tree($vocab->vid);
-		$topic_count = mydb::select('SELECT tid,COUNT(*) AS topics FROM %tag_topic% WHERE vid = :vid GROUP BY tid', ':vid',$vocab->vid);
+		$topic_count = DB::select([
+			'SELECT tid,COUNT(*) AS topics FROM %tag_topic% WHERE vid = :vid GROUP BY tid',
+			'var' => [':vid' => $vocab->vid]
+		]);
 		foreach ($topic_count->items as $topic) $topics[$topic->tid] = $topic->topics;
 	}
 
 	event_tricker('paper.listing.start',$self,$topics,$topicCondition);
 
-	if ($tree) {
-		$tables = new Table();
-		$tables->addId('forum-list');
-		$tables->thead = array(
-			'name' => 'Forum',
-			'topics -amt' => 'Topics',
-			'posts -amt' => 'Posts',
-			'lastpost -date' => 'Last post',
-		);
-
-		foreach ($tree as $term) {
-			$tables->rows[] = array(
-				str_repeat('--', $term->depth).'<a href="'.url('forum/'.$term->tid).'">'.$term->name.'</a>',
-				$topics[$term->tid],
-				'',
-				'',
-				'config' => array('class' => count($term->parents) == 1 ? 'containers' : null)
-			);
-		}
-		$ret .= $tables->build();
-	}
-
-	$ret .= '<h3>Last post topics</h3>';
-
 	$topics = PaperModel::items($topicCondition);
 
-	$ret .= (new PaperListWidget([
-		'id' => 'home-promote',
-		'class' => 'promote',
-		'listStyle' => $listStyle,
-		'url' => q(),
-		'order' => $getOrder,
-		'headerSortParameter' => ['listStyle' => $listStyle, 'page' => $page, 'items' => $getItems],
-		'children' => $topics->items,
-	]))->build();
+	$result = new Widget([
+		'children' => [
+			user_access('create forum content') ? '<nav class="nav -page -sg-text-right"><a class="btn -primary" href="'.url('paper/post/forum').'"><i class="icon -material">add_circle</i><span>{tr:Post new Forum topic}</span></a></nav>' : null,
+			$tree ? new Table([
+				'id' => 'forum-list',
+				'thead' => [
+					'name' => 'Forum',
+					'topics -amt' => 'Topics',
+					'posts -amt' => 'Posts',
+					'lastpost -date' => 'Last post',
+				],
+				'children' => array_map(
+					function($term) {
+						return [
+							str_repeat('--', $term->depth).'<a href="'.url('forum/'.$term->tid).'">'.$term->name.'</a>',
+							$topics[$term->tid],
+							'',
+							'',
+							'config' => array('class' => count($term->parents) == 1 ? 'containers' : null)
+						];
+					},
+					$tree
+				),
+			]) : null,
 
-
-	$ret .= $topics->page->show._NL;
+			'<h3>Last post topics</h3>',
+			new PaperListWidget([
+				'id' => 'home-promote',
+				'class' => 'promote',
+				'listStyle' => $listStyle,
+				'url' => q(),
+				'order' => $getOrder,
+				'headerSortParameter' => ['listStyle' => $listStyle, 'page' => $page, 'items' => $getItems],
+				'children' => $topics->items,
+			]),
+			$topics->page->show
+		]
+	]);
 
 	event_tricker('paper.listing.complete',$self,$topics,$topicCondition);
 
-	return $ret;
+	return $result;
 }
 ?>
