c9a98d7a549ba20e0a9ca76eb83c1a412e407d3e
Panumas <softganz@gmail.com>
2026-09-18 11:44:57 +0700

    refactor: fix deprecated

--- widget/widget.stat.php
+++ widget/widget.stat.php
@@ -3,8 +3,8 @@
  * Widget   :: Inline Statistic Widget
  * Author   :: Little Bear<softganz@gmail.com>
  * Created  :: 2011-11-04
- * Modified :: 2026-08-24
- * Version  :: 4
+ * Modified :: 2026-09-18
+ * Version  :: 5
  *
  * @param String $para
  * 	data-header=Header
@@ -43,12 +43,12 @@ function widget_stat($funcName = NULL, $para = NULL) {
 		. 'Web Statistics: '.($isAccessStat ? '</a>' : '')
 		. '</span>'
 		. '<span class="stat--current">Current <strong><a href="'.($isAccessStat ? url('stats') : '#').'" title="'.($isAccessStat ? $counter->online_name : '').'">'.$counter->online_members.'</a></strong> '
-		. 'members from <strong>'.number_format($counter->online_count).'</strong>persons online. </span>'
-		. '<span class="stat--today">Today <strong>'.number_format($today_hits->users).'</strong> persons <strong>'.number_format($today_hits->hits).'</strong> views. </span>'
-		. '<span class="stat--yesterday">Yesterday <strong>'.number_format($yesterday_hits->users).'</strong> persons '
-		. '<strong>'.number_format($yesterday_hits->hits).'</strong> views. </span>'
-		. '<span class="stat--total">Total view <strong>'.number_format($counter->users_count).'</strong> persons '
-		. '<strong>'.number_format($counter->hits_count).'</strong> views from <strong>'.number_format($counter->members).'</strong> members. '
+		. 'members from <strong>'.number_format($counter->online_count ?? 0).'</strong>persons online. </span>'
+		. '<span class="stat--today">Today <strong>'.number_format($today_hits->users ?? 0).'</strong> persons <strong>'.number_format($today_hits->hits).'</strong> views. </span>'
+		. '<span class="stat--yesterday">Yesterday <strong>'.number_format($yesterday_hits->users ?? 0).'</strong> persons '
+		. '<strong>'.number_format($yesterday_hits->hits ?? 0).'</strong> views. </span>'
+		. '<span class="stat--total">Total view <strong>'.number_format($counter->users_count ?? 0).'</strong> persons '
+		. '<strong>'.number_format($counter->hits_count).'</strong> views from <strong>'.number_format($counter->members ?? 0).'</strong> members. '
 		. 'Since '.sg_date($counter->created_date,'M,d Y')
 		. '.</span><!--stat-->';
 
