295dced49378d183500deb977efbad3b0aa14d9c
Panumas <softganz@gmail.com>
2026-09-26 14:36:52 +0700

    refactor: sign out to use user model

--- modules/system/page.signout.php
+++ modules/system/page.signout.php
@@ -1,15 +1,16 @@
 <?php
 /**
-* signout :: User sign out
-* Created :: 2021-12-12
-* Modify  :: 2025-06-15
-* Version :: 2
-*
-* @param String $args
-* @return Widget
-*
-* @usage project/helpcenter
-*/
+ * signout  :: User sign out
+ * Author   :: Little Bear<softganz@gmail.com>
+ * Created  :: 2021-12-12
+ * Modified :: 2026-09-26
+ * Version  :: 3
+ *
+ * @param String $args
+ * @return Widget
+ *
+ * @uses project/helpcenter
+ */
 
 import('model:user.php');
 
@@ -20,18 +21,9 @@ function signout() {
 		'message' => 'User '.i()->username.' was signout',
 	]);
 
-	UserModel::clearLogin();
-	Cache::Clear('user:'.$_COOKIE[cfg('cookie.id')]);
-
-	setcookie(cfg('cookie.id'),"",time() - 3600,cfg('cookie.path'),cfg('cookie.domain'));
-	setcookie(cfg('cookie.u'),"",time() - 3600,cfg('cookie.path'),cfg('cookie.domain'));
-
-	unset($_SESSION['logas']);
+	UserModel::signOutProcess();
 
 	$ret = isset($_GET['ret_url']) ? $_GET['ret_url'] : $_SERVER['HTTP_REFERER'];
-
-	session_unset();
-	session_destroy();
 
 	location($ret);
 }
